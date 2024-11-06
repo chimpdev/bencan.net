3d89ecfbe3d1d96211cabeb3432ab63d318e4d17
chimpdev <skyhancloud@gmail.com>
2024-11-06 07:11:24 +0300

    docs(cn): add JSDoc comments for cn utility function

--- utils/cn.ts
+++ utils/cn.ts
@@ -2,6 +2,12 @@ import { clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 import type { ClassValue } from '@/types/index';
 
+/**
+ * Combines multiple class names into a single string, merging Tailwind CSS classes when necessary.
+ *
+ * @param {...ClassValue[]} inputs - An array of class values to be combined.
+ * @returns {string} - A single string containing the combined class names.
+ */
 export default function cn(...inputs: ClassValue[]): string {
   return twMerge(clsx(inputs));
 }
